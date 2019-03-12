2c0e598b609b5ccd4b6d4213f98187e586ab34f7
Diego Diaz <diduscdm@gmail.com>
2019-03-12 20:17:09 +0100

    login y dashboard responsive

---
+++ gym-tool/src/main/java/com/diduslabs/gymtool/Item.java
@@ -0,0 +1,11 @@
+package com.diduslabs.gymtool;
+
+public class Item {
+	String name;
+	String description;
+	
+	Item(String name, String price) {
+	      this.name = name;
+	      this.description = price;
+	}
+}
--- gym-tool/src/main/java/com/diduslabs/gymtool/WebController.java
+++ gym-tool/src/main/java/com/diduslabs/gymtool/WebController.java
@@ -1,5 +1,8 @@
 package com.diduslabs.gymtool;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +10,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-	@GetMapping("/register")
+	@GetMapping("/logIn")
 	public String Hello() {
+		return "logIn.html";
+	}
+	
+	@GetMapping("/register")
+	public String Hello2() {
 		return "register.html";
 	}
 	
-	@GetMapping("/hello-mustache")
+	@GetMapping("/dashboard")
 	public String HelloTemplates(Model model) {
-		model.addAttribute("name","Pedro");
-		return "hello-templates";
+		List<Item> listAnything = new ArrayList<Item>();
+		listAnything.add(new Item("caca","323232"));
+		listAnything.add(new Item("Pepe","2"));
+		
+		model.addAttribute("isEmptyFavRoutine",listAnything.isEmpty());
+		model.addAttribute("isEmptyClass",listAnything.isEmpty());
+		
+		model.addAttribute("routine",listAnything);
+		model.addAttribute("class",listAnything);
+		return "dashboard";
 	}
 }
